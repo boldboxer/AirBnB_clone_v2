a52a4355cdf0ae098411e79f8775c752199d1c22
boldboxer <ebesteinpr@gmail.com>
2023-08-25 00:06:12 -0700

    tests

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -17,3 +17,27 @@ class test_state(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+class TestDatabaseChanges(unittest.TestCase):
+    def setUp(self):
+        # Prepare the test environment (e.g., set up database fixtures)
+
+    def tearDown(self):
+        # Clean up after the test (e.g., remove temporary data)
+
+    def test_create_state(self):
+        # Get the initial count of records
+        initial_count = self.get_state_count()
+
+        # Execute the console command to create a state (simulating user interaction)
+        # You can use subprocess or similar method to run console commands
+
+        # Get the count of records after the command is executed
+        final_count = self.get_state_count()
+
+        # Validate the change in count
+        self.assertEqual(final_count, initial_count + 1)
+    
+    def get_state_count(self):
+        # Implement a method to retrieve the count of records from the states table
+
